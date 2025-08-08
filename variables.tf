5b25807b1b63784ba1574d9813bd8d59a699a7d3
peetnavapon <peetnavaponchaovanon@gmail.com>
2025-08-08 13:23:20 +0700

    update variable
    เปลี่ยนชื่อตัวแปรและเพิ่มตัวแปรที่ใช้

--- variables.tf
+++ variables.tf
@@ -1,18 +1,43 @@
-variable "cloud_secret_id"{
+variable "cloud_secret_id" {
+  description = "Tencent Cloud Secret ID"
   type        = string
   sensitive   = true
-  description = "Tencent Cloud Secret ID"
 }
 
 variable "cloud_secret_key" {
+  description = "Tencent Cloud Secret Key"
   type        = string
   sensitive   = true
-  description = "Tencent Cloud Secret Key"
 }
 
-variable "cloud_region" {
+variable "region" {
+  description = "Tencent Cloud region"
   type        = string
-  description = "Tencent Cloud Region"
+  default     = "ap-bangkok"
+}
+
+variable "availability_zone" {
+  description = "Availability zone in the region"
+  type        = string
+  default     = "ap-bangkok-1"
+}
+
+variable "image_id" {
+  description = "Tencent Cloud Ubuntu image ID"
+  type        = string
+  default     = "img-pi0ii46r"
+}
+
+variable "instance_type" {
+  description = "Tencent Cloud instance type"
+  type        = string
+  default     = "S3.MEDIUM4"
+}
+
+variable "vm_password" {
+  description = "Password for the VM login"
+  type        = string
+  sensitive   = true
 }
 
 variable "db_password" {
